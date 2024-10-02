205939caba5fbb6dccf522b4d923593de8008800
ShaynePark22 <smtt22@gmail.com>
2024-10-02 20:03:23 +0900

    add jh test2

--- tiggle/src/main/java/com/ex/tiggle/orgregist/controller/OrgregistController.java
+++ tiggle/src/main/java/com/ex/tiggle/orgregist/controller/OrgregistController.java
@@ -3,5 +3,5 @@ package com.ex.tiggle.orgregist.controller;
 public class OrgregistController {
 /////////////////////hjhjhjhjhj
 	private String jhjh;
-	private String sisi;
+	private String aaaa;
 }
